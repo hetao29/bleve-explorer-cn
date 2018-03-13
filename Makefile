66388565e8861426e52ffd3665f1fd1c2db18e76
hetao <hetao@hetao.name>
2018-03-13 19:10:24 +0800

    update dict

--- Makefile
+++ Makefile
@@ -1,2 +1,6 @@
 all:
-	go build
+	go build -o bin/bleve-explorer-cn
+start:
+	./bin/bleve-explorer-cn >> log/explorer.log 2>&1 &
+stop:
+	killall bleve-explorer-cn
--- main.go
+++ main.go
@@ -10,15 +10,19 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"os/exec"
+	"path/filepath"
 	"runtime"
 
 	"github.com/gorilla/mux"
-	_ "github.com/xgdapg/daemon"
+	"github.com/xgdapg/daemon"
 
 	"github.com/blevesearch/bleve"
 	bleveHttp "github.com/blevesearch/bleve/http"
@@ -29,8 +33,8 @@ import (
 	_ "github.com/hetao29/blevesearch-cn/scws/bleve"
 )
 
-var bindAddr = flag.String("addr", ":8095", "http listen address")
-var dataDir = flag.String("dataDir", "data", "data directory")
+//var bindAddr = flag.String("addr", ":8095", "http listen address")
+//var dataDir = flag.String("dataDir", "data", "data directory")
 var staticEtag = flag.String("staticEtag", "", "optional static etag value.")
 var staticPath = flag.String("static", "",
 	"optional path to static directory for web resources")
@@ -40,19 +44,66 @@ var staticBleveMappingPath = flag.String("staticBleveMapping", "",
 var configFile = flag.String("c", "etc/config.json", "config filename")
 var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
 
+func getFilePath(filename string) string {
+	file, _ := exec.LookPath(os.Args[0])
+	path, _ := filepath.Abs(filepath.Base(file))
+	curdir := filepath.Dir(path)
+	return fmt.Sprintf("%s/%s", curdir, filename)
+}
+
+type Config struct {
+	BindAddr string `json:"addr"`
+	DataDir  string `json:"dataDir"`
+	Daemon   bool   `json:"daemon"`
+}
+
+func LoadConfig(configFile string) Config {
+	var config = Config{}
+	data, err := ioutil.ReadFile(configFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("load config file %s \n%s\n", configFile, string(data))
+	if err := json.Unmarshal(data, &config); err != nil {
+		log.Println("config file parse error!")
+		log.Fatal(err)
+	}
+	log.Println(config.Daemon)
+	if config.BindAddr == "" {
+		log.Fatal("addr can not empty!")
+	}
+	if config.DataDir == "" {
+		log.Fatal("DataDir can not empty!")
+	}
+	return config
+}
+
 func main() {
 	flag.Parse()
 
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
+	var realConfig = getFilePath(*configFile)
+	config := LoadConfig(realConfig)
+	log.Println("Config filename is ", realConfig)
+	defer func() { // 必须要先声明defer，否则不能捕获到panic异常
+		if err := recover(); err != nil {
+			fmt.Println(err) // 这里的err其实就是panic传入的内容，55
+		}
+	}()
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	if config.Daemon {
+		daemon.Exec(daemon.Daemon)
+	}
+
 	// walk the data dir and register index names
-	dirEntries, err := ioutil.ReadDir(*dataDir)
+	dirEntries, err := ioutil.ReadDir(config.DataDir)
 	if err != nil {
 		log.Fatalf("error reading data dir: %v", err)
 	}
 
 	for _, dirInfo := range dirEntries {
-		indexPath := *dataDir + string(os.PathSeparator) + dirInfo.Name()
+		indexPath := config.DataDir + string(os.PathSeparator) + dirInfo.Name()
 
 		// skip single files in data dir since a valid index is a directory that
 		// contains multiple files
@@ -72,8 +123,6 @@ func main() {
 		}
 	}
 
-	runtime.GOMAXPROCS(runtime.NumCPU())
-	//daemon.Exec(daemon.Daemon)
 
 	router := mux.NewRouter()
 	router.StrictSlash(true)
@@ -108,7 +157,7 @@ func main() {
 	// add the API
 	bleveMappingUI.RegisterHandlers(router, "/api")
 
-	createIndexHandler := bleveHttp.NewCreateIndexHandler(*dataDir)
+	createIndexHandler := bleveHttp.NewCreateIndexHandler(config.DataDir)
 	createIndexHandler.IndexNameLookup = indexNameLookup
 	router.Handle("/api/{indexName}", createIndexHandler).Methods("PUT")
 
@@ -116,7 +165,7 @@ func main() {
 	getIndexHandler.IndexNameLookup = indexNameLookup
 	router.Handle("/api/{indexName}", getIndexHandler).Methods("GET")
 
-	deleteIndexHandler := bleveHttp.NewDeleteIndexHandler(*dataDir)
+	deleteIndexHandler := bleveHttp.NewDeleteIndexHandler(config.DataDir)
 	deleteIndexHandler.IndexNameLookup = indexNameLookup
 	router.Handle("/api/{indexName}", deleteIndexHandler).Methods("DELETE")
 
@@ -160,6 +209,6 @@ func main() {
 
 	// start the HTTP server
 	http.Handle("/", router)
-	log.Printf("Listening on %v", *bindAddr)
-	log.Fatal(http.ListenAndServe(*bindAddr, nil))
+	log.Printf("Listening on %v", config.BindAddr)
+	log.Fatal(http.ListenAndServe(config.BindAddr, nil))
 }
